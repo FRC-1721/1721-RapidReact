1ce3e6f07a8852c3a4b94097af8d33e4cb2ea8b3
KenwoodFox <kenwood364@gmail.com>
2022-01-21 20:01:34 -0500

    This should fix black suggestions

--- src/robot.py
+++ src/robot.py
@@ -19,14 +19,14 @@ class UnnamedToaster(wpilib.TimedRobot):
         self.starboardMotorTwo.follow(self.starboardMotorOne)
 
         self.driveTrain = wpilib.drive.DifferentialDrive(
-            self.portMotorLeader, self.starboardMotorOne)
+            self.portMotorLeader, self.starboardMotorOne
+        )
 
         self.joy = wpilib.Joystick(0)
 
     # Initalizes joystick to control the drivetrain
     def teleopPeriodic(self):
-        self.driveTrain.arcadeDrive(
-            self.joy.getRawAxis(1), -self.joy.getRawAxis(2))
+        self.driveTrain.arcadeDrive(self.joy.getRawAxis(1), -self.joy.getRawAxis(2))
 
 
 if __name__ == "__main__":
