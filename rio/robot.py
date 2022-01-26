c08e9aa5a31bc71c91e6e0fb392b784062072869
KenwoodFox <kenwood364@gmail.com>
2022-01-25 20:33:10 -0500

    Test swerve 4 wheel kinematics

--- rio/robot.py
+++ rio/robot.py
@@ -1,5 +1,6 @@
 import wpilib
 import wpilib.drive
+import wpimath
 import rev
 
 
@@ -8,15 +9,41 @@ class UnnamedToaster(wpilib.TimedRobot):
 
         # Initialize port side drive train
         # Port wheel
-        self.portMotorLeader = rev.CANSparkMax(2, rev.MotorType.kBrushless)
-        self.portMotorFollower = rev.CANSparkMax(1, rev.MotorType.kBrushless)
+        self.portMotorLeader = rev.CANSparkMax(
+            2, rev._rev.CANSparkMaxLowLevel.MotorType.kBrushless
+        )
+        self.portMotorFollower = rev.CANSparkMax(
+            1, rev._rev.CANSparkMaxLowLevel.MotorType.kBrushless
+        )
 
         # Starboard wheel
-        self.starboardMotorOne = rev.CANSparkMax(4, rev.MotorType.kBrushless)
-        self.starboardMotorTwo = rev.CANSparkMax(3, rev.MotorType.kBrushless)
+        self.starboardMotorOne = rev.CANSparkMax(
+            4, rev._rev.CANSparkMaxLowLevel.MotorType.kBrushless
+        )
+        self.starboardMotorTwo = rev.CANSparkMax(
+            3, rev._rev.CANSparkMaxLowLevel.MotorType.kBrushless
+        )
 
         self.portMotorFollower.follow(self.portMotorLeader)
         self.starboardMotorTwo.follow(self.starboardMotorOne)
+
+        # Experimenting with swerve drive
+
+        self.ForeStarboardSwerveModuleLocation = wpimath.geometry.Translation2d(
+            0.5, -0.5
+        )
+        self.AftStarboardSwerveModuleLocation = wpimath.geometry.Translation2d(
+            -0.5, -0.5
+        )
+        self.ForePortSwerveModuleLocation = wpimath.geometry.Translation2d(0.5, 0.5)
+        self.AftPortSwerveModuleLocation = wpimath.geometry.Translation2d(-0.5, 0.5)
+
+        self.swerveDrivetrain = wpimath.kinematics.SwerveDrive4Kinematics(
+            self.ForeStarboardSwerveModuleLocation,
+            self.AftStarboardSwerveModuleLocation,
+            self.ForePortSwerveModuleLocation,
+            self.AftPortSwerveModuleLocation,
+        )
 
         self.driveTrain = wpilib.drive.DifferentialDrive(
             self.portMotorLeader, self.starboardMotorOne
@@ -27,6 +54,27 @@ class UnnamedToaster(wpilib.TimedRobot):
     # Initalizes joystick to control the drivetrain
     def teleopPeriodic(self):
         self.driveTrain.arcadeDrive(self.joy.getRawAxis(1), -self.joy.getRawAxis(2))
+
+        m_chassisSpeeds = wpimath.kinematics.ChassisSpeeds(
+            self.joy.getRawAxis(1), self.joy.getRawAxis(0), self.joy.getRawAxis(2)
+        )
+
+        fl, fr, bl, br = self.swerveDrivetrain.toSwerveModuleStates(m_chassisSpeeds)
+
+        print(
+            f"""Module A
+        {fl.speed}
+        {fl.angle}
+        Module B
+        {fr.speed}
+        {fr.angle}
+        Module C
+        {bl.speed}
+        {bl.angle}
+        Module D
+        {br.speed}
+        {br.angle}"""
+        )
 
 
 if __name__ == "__main__":
