f1e49443d2f43e003cf841269163bccb3b459e76
Keegan <kredcool@outlook.com>
2022-02-16 16:15:02 -0500

    made base vision file

---
+++ rio/autonomous/vision.py
@@ -0,0 +1,15 @@
+import commands2
+import constants
+from networktables import NetworkTables
+from subsystems.drivetrain import Drivetrain
+from subsystems.yoke import Yoke
+
+
+class vision(commands2.SequentialCommandGroup):
+    """
+    A vision command
+    """
+
+    def __init__(self, drive: Drivetrain, yoke: Yoke):
+        super().__init__()
+        NetworkTables.getTable("limelight").getNumber("<variablename>")
