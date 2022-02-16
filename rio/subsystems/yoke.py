86016ecb9a6f5e5045200c47de871118e6c4a820
Khan Simeoni <simeonikh@gmail.com>
2022-02-15 19:09:58 -0500

    fixedq
    :q
    
    x

--- rio/subsystems/yoke.py
+++ rio/subsystems/yoke.py
@@ -4,6 +4,7 @@
 from commands2 import SubsystemBase
 
 from ctre import TalonFX, ControlMode
+from rev import CANSparkMax, CANSparkMaxLowLevel
 
 from constants.constants import getConstants
 
@@ -24,11 +25,16 @@ class Yoke(SubsystemBase):
         # Configure all motors
         self.star_shooter = TalonFX(self.yoke_const["shooter"]["star_falcon_id"])
         self.port_shooter = TalonFX(self.yoke_const["shooter"]["port_falcon_id"])
-        self.upperdowner = CANSparkMax(self.yoke_const["shooter"]["upper_downer_id"], CANSparkMaxLowLevel.MotorType.kBrushless)
-        
-        #this may not be the way to do it, please ckeck later
-        self.kicky = CANSparkMax(self.yoke_const["shooter"]["kicky_id"], CANSparkMaxLowLevel.MotorType.kBrushless)
+        self.upperdowner = CANSparkMax(
+            self.yoke_const["shooter"]["upper_downer_id"],
+            CANSparkMaxLowLevel.MotorType.kBrushless,
+        )
 
+        # this may not be the way to do it, please ckeck later
+        self.kicky = CANSparkMax(
+            self.yoke_const["shooter"]["kicky_id"],
+            CANSparkMaxLowLevel.MotorType.kBrushless,
+        )
 
     def driveSloppy(self, speed):
         """
@@ -42,34 +48,37 @@ class Yoke(SubsystemBase):
         self.port_shooter.set(ControlMode.PercentOutput, -speed)
 
         print(speed)
-    
-    def raiseDropSloppy(self, degs, curDegs): #raises or lowers the shooter the inputted amount of degrees
-        #degs is the amount to move, curDegs is the degrees off the ground it already is
-        if(curDegs+degs<=100 and curDegs+degs>=0): #stops the shooter from going underground or into itself
-            self.upperdowner.set(degs) #moves the shooter
-            return curDegs+degs #returns the updated degrees
+
+    def raiseDropSloppy(
+        self, degs, curDegs
+    ):  # raises or lowers the shooter the inputted amount of degrees
+        # degs is the amount to move, curDegs is the degrees off the ground it already is
+        if (
+            curDegs + degs <= 100 and curDegs + degs >= 0
+        ):  # stops the shooter from going underground or into itself
+            self.upperdowner.set(degs)  # moves the shooter
+            return curDegs + degs  # returns the updated degrees
         else:
-            if(degs>0):
-                self.upperdowner.set(100-curDegs) #moves the shooter
-                return 100 #returns maximum
+            if degs > 0:
+                self.upperdowner.set(100 - curDegs)  # moves the shooter
+                return 100  # returns maximum
             else:
-                self.upperdowner.set(-curDegs) #moves the shooter
-                return 0 #returns minimum
+                self.upperdowner.set(-curDegs)  # moves the shooter
+                return 0  # returns minimum
 
-    def kickyGoSloppy(self, degs, curDegs): #raises or lowers the kicky the inputted amount of degrees
-        #degs is the amount to move, curDegs is the degrees off the ground it already is
-        if(curDegs+curDegs<=60 and curDegs+curDegs>=0): #stops the kicky from going underground or into itself
-            self.kicky.set(degs) #moves the kicky
-            return curDegs+degs  #returns the updated degrees
+    def kickyGoSloppy(
+        self, degs, curDegs
+    ):  # raises or lowers the kicky the inputted amount of degrees
+        # degs is the amount to move, curDegs is the degrees off the ground it already is
+        if (
+            curDegs + curDegs <= 60 and curDegs + curDegs >= 0
+        ):  # stops the kicky from going underground or into itself
+            self.kicky.set(degs)  # moves the kicky
+            return curDegs + degs  # returns the updated degrees
         else:
-             if(degs>0):
-                self.kicky.set(60-curDegs) #moves the kicky
-                return 60 #returns maximum
+            if degs > 0:
+                self.kicky.set(60 - curDegs)  # moves the kicky
+                return 60  # returns maximum
             else:
-                self.kicky.set(-curDegs) #moves the kicky
-                return 0 #returns minimum
-        
-
-
-
-
+                self.kicky.set(-curDegs)  # moves the kicky
+                return 0  # returns minimum
