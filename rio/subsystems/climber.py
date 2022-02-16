10c67361e8efdb5f986ffbc80728fc8f596c6315
Khan Simeoni <simeonikh@gmail.com>
2022-02-15 19:03:13 -0500

    climber code is ddont, not really, but its as good as we can get without the hardware

--- rio/subsystems/climber.py
+++ rio/subsystems/climber.py
@@ -2,7 +2,9 @@
 # 2022
 
 # TODO get only what we need to initalize motors
-import rev
+import wpilib
+
+from rev import CANSparkMaxLowLevel, CANSparkMax
 from constants.constants import getConstants
 from commands2 import Subsystem, SubsystemBase
 
@@ -21,10 +23,19 @@ class Climber(SubsystemBase):
         self.Climber_const = constants["Climber"]
 
         # motor configuration
-        self.portClimber = rev.CANSparkMax(1, rev.MotorType.kBrushless)
-        self.starboardClimber = rev.CANSparkMax(2, rev.MotorType.kBrushless)
+        self.portClimber = CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless)
+        self.starboardClimber = CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless)
 
         self.starboardClimber.follow(self.portClimber, True)
+
+        self.backgroundTimer = wpilib.Timer()
+        self.backgroundTimer.start()
+
+        self.climbPID = self.portClimber.getPIDController()
+
+        self.climbPID.setI(1)
+        self.climbPID.setP(1)
+        self.climbPID.setD(1)
 
     def climb(self, speed):
         """
@@ -32,6 +43,6 @@ class Climber(SubsystemBase):
         to climb
         """
 
-        self.portClimber.SetReference(10)
+        self.climbPID.setReference(100, CANSparkMaxLowLevel.ControlType.kPosition)
         self.backgroundTimer.hasPeriodPassed(5)
-        self.portClimber.SetReference(10)
+        self.climbPID.setReference(0)
