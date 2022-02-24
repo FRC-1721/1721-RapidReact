77269e1d6a7c24136f3b68df362d354534e9a5d9
KenwoodFox <31870999+KenwoodFox@users.noreply.github.com>
2022-02-24 17:50:58 -0500

    Add reticle display to dashboard (#45)
    Thanks @KhanSimeoni

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -5,10 +5,10 @@ import updateTemp from "./temp-display";
 import { updateValue } from "./utils";
 import { setupPID, receivePIDUpdate } from "./pid-optimizer";
 
-const WHEEL_PORT_X = 25 + 50/2;
-const WHEEL_STARBOARD_X = 200 + 50/2;
-const WHEEL_FORE_Y = 25 + 75/2;
-const WHEEL_AFT_Y = 200 + 75/2;
+const WHEEL_PORT_X = 25 + 50 / 2;
+const WHEEL_STARBOARD_X = 200 + 50 / 2;
+const WHEEL_FORE_Y = 25 + 75 / 2;
+const WHEEL_AFT_Y = 200 + 75 / 2;
 
 // make these packages visible to the networktables helpers, too
 window.$ = $;
@@ -33,12 +33,12 @@ $(document).ready(function () {
     loadCameraOnConnect({
         container: '#camera_container', // where to put the img tag
         proto: null,                    // optional, defaults to http://
-        host: null,                     // optional, if null will use robot's autodetected IP address
-        port: 5800,                     // webserver port
-        image_url: 'http://10.17.21.11:8000/video',   // mjpg stream of camera
-        data_url: '/program.json',      // used to test if connection is up
-        wait_img: null,                 // optional img to show when not connected, can use SVG instead
-        error_img: null,                // optional img to show when error connecting, can use SVG instead
+        host: "10.17.21.11",                     // optional, if null will use robot's autodetected IP address
+        port: 5801,                     // webserver port
+        image_url: '/video',   // mjpg stream of camera
+        data_url: '/',      // used to test if connection is up
+        wait_img: 'resources/no_signal.png',                 // optional img to show when not connected, can use SVG instead
+        error_img: 'resources/error.png',                // optional img to show when error connecting, can use SVG instead
         attrs: {                        // optional: attributes set on svg or img element
             width: 320,                     // optional, stretches image to this width
             height: 240,                    // optional, stretches image to this width
@@ -49,25 +49,25 @@ $(document).ready(function () {
     if (canvas.getContext) {
         // draw initial wheels
         drawingContext = canvas.getContext('2d');
-        drawWheel(WHEEL_PORT_X, WHEEL_FORE_Y, 0);
-        drawWheel(WHEEL_STARBOARD_X, WHEEL_FORE_Y, 0);
-        drawWheel(WHEEL_PORT_X, WHEEL_AFT_Y, 0);
-        drawWheel(WHEEL_STARBOARD_X, WHEEL_AFT_Y, 0);
+        drawModule(WHEEL_PORT_X, WHEEL_FORE_Y, 0, 0);
+        drawModule(WHEEL_STARBOARD_X, WHEEL_FORE_Y, 0, 0);
+        drawModule(WHEEL_PORT_X, WHEEL_AFT_Y, 0, 0);
+        drawModule(WHEEL_STARBOARD_X, WHEEL_AFT_Y, 0, 0);
     }
 
     setupPID();
 });
 
-function drawWheel(x, y, angle) {
+function drawModule(x, y, real_angle, target_angle) {
     if (drawingContext) {
-        drawingContext.clearRect(x-75, y-75, 150, 150);
+        drawingContext.clearRect(x - 75, y - 75, 150, 150);
         //save the untranslated/unrotated context
         drawingContext.save();
         drawingContext.beginPath();
         //move the rotation point to the center of the rect
         drawingContext.translate(x, y);
         //rotate the rect
-        drawingContext.rotate(angle);
+        drawingContext.rotate(real_angle);
         //draw the rect on the transformed context
         //Note: after transforming [0,0] is visually [x,y]
         //so the rect needs to be offset accordingly when drawn
@@ -75,29 +75,42 @@ function drawWheel(x, y, angle) {
         drawingContext.fill();
         //restore the context to its untranslated/unrotated state
         drawingContext.restore();
+
+        // The same again for the target
+        drawingContext.save();
+        drawingContext.beginPath();
+        drawingContext.translate(x, y);
+        drawingContext.rotate(target_angle);
+        drawingContext.drawImage(swerveReticleImage, -39 / 2, -85 / 2);
+        drawingContext.fill();
+        drawingContext.restore();
     }
 }
 
 var swerveDriveImage = new Image();
 swerveDriveImage.src = 'resources/swerveModule.png'
+
+var swerveReticleImage = new Image();
+swerveReticleImage.src = 'resources/swerveReticle.png'
 function swerveListener(key, value, isNew) {
     // console.log(key)
-    if (key == "/SmartDashboard/SwerveDrive/fp_actual") {
-        drawWheel(WHEEL_PORT_X, WHEEL_FORE_Y, value);
+    if (key.includes("/SmartDashboard/SwerveDrive/fp_")) {
+        drawModule(WHEEL_PORT_X, WHEEL_FORE_Y, NetworkTables.getValue("/SmartDashboard/SwerveDrive/fp_actual"), NetworkTables.getValue("/SmartDashboard/SwerveDrive/fp_target"));
     }
-    if (key == "/SmartDashboard/SwerveDrive/fs_actual") {
-        drawWheel(WHEEL_STARBOARD_X, WHEEL_FORE_Y, value);
+    if (key.includes("/SmartDashboard/SwerveDrive/fs_")) {
+        drawModule(WHEEL_STARBOARD_X, WHEEL_FORE_Y, NetworkTables.getValue("/SmartDashboard/SwerveDrive/fs_actual"), NetworkTables.getValue("/SmartDashboard/SwerveDrive/fs_target"));
     }
-    if (key == "/SmartDashboard/SwerveDrive/ap_actual") {
-        drawWheel(WHEEL_PORT_X, WHEEL_AFT_Y, value);
+    if (key.includes("/SmartDashboard/SwerveDrive/ap_")) {
+        drawModule(WHEEL_PORT_X, WHEEL_AFT_Y, NetworkTables.getValue("/SmartDashboard/SwerveDrive/ap_actual"), NetworkTables.getValue("/SmartDashboard/SwerveDrive/ap_target"));
     }
-    if (key == "/SmartDashboard/SwerveDrive/as_actual") {
-        drawWheel(WHEEL_STARBOARD_X, WHEEL_AFT_Y, value);
+    if (key.includes("/SmartDashboard/SwerveDrive/as_")) {
+        drawModule(WHEEL_STARBOARD_X, WHEEL_AFT_Y, NetworkTables.getValue("/SmartDashboard/SwerveDrive/as_actual"), NetworkTables.getValue("/SmartDashboard/SwerveDrive/as_target"));
     }
+
     if (key.includes("/SmartDashboard/Thermals/")) {
         updateTemp(key, value);
     }
-    if (key.includes("/SmartDashboard/PID/") ) {
+    if (key.includes("/SmartDashboard/PID/")) {
         receivePIDUpdate(key, value);
     }
 }
