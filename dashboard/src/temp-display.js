fc50f5c648166332d67c260ec005914a7903d890
KenwoodFox <31870999+KenwoodFox@users.noreply.github.com>
2022-03-03 00:07:54 -0500

    Audio Cues (#49)

--- dashboard/src/temp-display.js
+++ dashboard/src/temp-display.js
@@ -2,6 +2,10 @@ const RED_TEMP = 80;
 const YELLOW_TEMP = 60;
 const RED = "#FFBBBB"
 const YELLOW = "#FFE900"
+
+
+var criticalAlarm = new Audio("resources/Critical_Alarm.mp3");
+
 function updateTemp(key, value) {
     const splitKey = key.split("/");
     const id = splitKey[splitKey.length - 1];
@@ -11,6 +15,7 @@ function updateTemp(key, value) {
             elem.innerHTML = `${value} C`;
             if (value > RED_TEMP) {
                 elem.style.backgroundColor = RED;
+                criticalAlarm.play();
             } else if (value > YELLOW_TEMP) {
                 elem.style.backgroundColor = YELLOW;
             } else {
