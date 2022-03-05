9c6689549cb27c2dbb6040b66baccb5c338c7bcc
KenwoodFox <kenwood364@gmail.com>
2022-03-05 09:07:21 -0500

    Add high auto too

--- rio/autonomous/highBotchAuto.py
+++ rio/autonomous/highBotchAuto.py
@@ -9,7 +9,7 @@ from subsystems.drivetrain import Drivetrain
 
 from commands.kicker_button import Kicker
 from commands.catapult import Catapult
-from commands.flybywire import FlyByWire
+from commands.flywithwires import FlyWithWires
 from commands.zero_swerve import ZeroSwerveModules
 
 
@@ -24,4 +24,5 @@ class HighBotchAuto(commands2.SequentialCommandGroup):
             Catapult(yoke, 78.5, 0.52, True),  # Shoot like dis
             WaitCommand(2),  # Wait again
             Kicker(yoke),
+            FlyWithWires(drivetrain, fwd=-0.2, time=2),
         )
