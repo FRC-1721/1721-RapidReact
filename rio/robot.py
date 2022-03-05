9e815c88d9dc2940d02fcd9a78f31b2cd51c23ee
Keegan <kredcool@outlook.com>
2022-03-05 08:00:47 -0500

    the robot code should run now, this will need to be done on a robot heavy branch because it doesn't send the messege if it isn't. We could also make a discord bot to detect when the sim fails and ping us

--- rio/robot.py
+++ rio/robot.py
@@ -8,7 +8,7 @@ from robotcontainer import RobotContainer
 from datetime import datetime
 
 
-class verybadbroken(commands2.TimedCommandRobot):
+class BurntToaster(commands2.TimedCommandRobot):
     """
     Our default robot class, pass it to wpilib.run
 
