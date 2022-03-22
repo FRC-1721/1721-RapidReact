340b8fd23ded4c7f5f0dad1d339140e282698473
Keegan <kredcool@outlook.com>
2022-03-22 15:30:43 -0400

    moving files to proper place

---
+++ rio/commands/nullauto.py
@@ -0,0 +1,31 @@
+import math
+import commands2
+
+from commands.flybywire import FlyByWire
+
+
+class NullAuto(commands2.CommandBase):
+    def __init__(self, drivetrain) -> None:
+        """
+        Fill this out.
+        """
+        super().__init__()
+
+        self.drivetrain = drivetrain
+        self.angle = 0
+
+        self.addRequirements([self.drivetrain])
+
+    def execute(self) -> None:
+        self.drivetrain.arcadeDrive(
+            math.sin(self.angle) / 10,
+            math.cos(self.angle) / 10,
+            0,
+        )
+
+        if self.angle <= 360:
+            self.angle = self.angle + 0.1
+        else:
+            self.angle = 0
+
+        print(self.angle)
