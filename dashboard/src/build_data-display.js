89290582b5b863cfecb411b4fe008c50df7990ab
KenwoodFox <kenwood364@gmail.com>
2022-04-09 14:46:51 -0400

    More dashboard features

--- dashboard/src/build_data-display.js
+++ dashboard/src/build_data-display.js
@@ -1,3 +1,9 @@
+const RED = "#FFBBBB"
+const KHAN = "#fcec03"
+const JOE = "#03fce3"
+const KEEGAN = "#1c03fc"
+const JACK = "#fc03c6"
+
 function updateBuildData(key, value) {
     const splitKey = key.split("/");
     const id = splitKey[splitKey.length - 1];
@@ -8,7 +14,15 @@ function updateBuildData(key, value) {
 
             // Check if value is dirty
             if (value.includes("-dirty")) {
-                elem.style.backgroundColor = "#FFBBBB";
+                elem.style.backgroundColor = RED;
+            } else if (value.includes("joe")) {
+                elem.style.backgroundColor = JOE;
+            } else if (value.includes("khan")) {
+                elem.style.backgroundColor = KHAN;
+            } else if (value.includes("keegan")) {
+                elem.style.backgroundColor = KEEGAN;
+            } else if (value.includes("jack")) {
+                elem.style.backgroundColor = JACK;
             } else {
                 elem.style.backgroundColor = "transparent";
             }
